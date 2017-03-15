48e350db08ef43dce5a76f7f90ff30e198239332
Markus Mandalka <github@mandalka.name>
2017-03-15 08:08:39 +0100

    API

---
+++ src/api/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, url
+
+from api import views
+
+urlpatterns = patterns('',
+	url(r'^delete', 'api.views.queue_delete', name='queue_delete'),
+	url(r'^enrich', 'api.views.queue_enrich', name='queue_enrich'),
+	url(r'^index-web', 'api.views.queue_index_web', name='queue_index_web'),
+	url(r'^index-rss', 'api.views.queue_index_rss', name='queue_index_rss'),
+	url(r'^index-file', 'api.views.queue_index_file', name='queue_index_file'),
+	url(r'^index-dir', 'api.views.queue_index_file', name='queue_index_dir'),
+)
---
+++ src/api/views.py
@@ -0,0 +1,101 @@
+from django.shortcuts import render
+
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+import json
+
+from etl.tasks import enrich
+from etl.tasks import index_file
+from etl.tasks import index_web
+from etl.tasks import index_rss
+
+
+#
+# add delete task to queue
+#
+
+def queue_delete(request):
+	uri = request.GET["uri"]
+
+	if 'wait' in request.GET:
+		wait = int(request.GET['wait'])
+	else:
+		wait = 0
+
+	result = delete.delay(uri=uri)
+	
+	return HttpResponse(json.dumps( {'queue': result.id} ), content_type="application/json")
+
+
+#
+# add data enrichment / plugin processing task to queue
+#
+
+def queue_enrich(request):
+
+	uri = request.GET["uri"]
+
+	plugins = request.GET['plugins']
+
+	if 'wait' in request.GET:
+		wait = int(request.GET['wait'])
+	else:
+		wait = 0
+
+	result = enrich.delay(plugins = plugins, uri=uri, wait = wait )
+	
+	return HttpResponse(json.dumps( {'queue': result.id} ), content_type="application/json")
+
+
+#
+# add file indexing task to queue
+#
+
+def queue_index_file(request):
+
+	uri = request.GET["uri"]
+
+	if 'wait' in request.GET:
+		wait = int(request.GET['wait'])
+	else:
+		wait = 0
+
+	result = index_file.delay(filename=uri, wait = wait )
+	
+	return HttpResponse(json.dumps( {'queue': result.id} ), content_type="application/json")
+
+
+#
+# add web page indexing task to queue
+#
+
+def queue_index_web(request):
+	uri = request.GET["uri"]
+
+	if 'wait' in request.GET:
+		wait = int(request.GET['wait'])
+	else:
+		wait = 0
+
+	result = index_web.delay(uri=uri, wait=wait)
+	
+	return HttpResponse(json.dumps( {'queue': result.id} ), content_type="application/json")
+
+
+#
+# add RSS feed indexing task to queue
+#
+
+def queue_index_rss(request):
+	uri = request.GET["uri"]
+
+	if 'wait' in request.GET:
+		wait = int(request.GET['wait'])
+	else:
+		wait = 0
+
+	result = index_rss.delay(uri=uri, wait=wait)
+	
+	return HttpResponse(json.dumps( {'queue': result.id} ), content_type="application/json")
