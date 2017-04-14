db30a303b5cbf512f4c5197aa8efe29b027d9f7c
Markus Mandalka <github@mandalka.name>
2017-04-14 15:54:44 +0200

    Web UI for managing datasources for indexing

---
+++ src/datasources/views.py
@@ -0,0 +1,58 @@
+from django.http import HttpResponse 
+from django.shortcuts import render
+
+from etl.etl_web import Connector_Web
+
+from django import forms
+
+
+class ListForm(forms.Form):
+	list = forms.CharField(widget=forms.Textarea)
+	
+
+def index(request):
+
+	verbose = False
+
+	if request.method == 'POST': # If the form has been submitted...
+		form = ListForm(request.POST) # A form bound to the POST data
+		if form.is_valid():
+
+			list = form.cleaned_data['list']
+			list = list.split()
+
+			count = 0
+			error_messages = []
+
+			# exclude this urls
+			exclude = ['rect']
+
+			connector = Connector_Web(verbose=verbose)
+
+			# search all the lines
+			for uri in list:
+
+				uri=uri.strip()
+
+				if uri and not uri in exclude:
+					try:
+						connector.index(uri)
+						count = count + 1
+					except BaseException as e:
+						error_messages.append("Exception while ETL process of {} : {}".format( uri, str(e) ) )
+				
+
+			return render(request, 'crawl.html', 
+				{	
+					"form": form,
+					"count": count,
+					"error_messages": error_messages,
+				})
+
+		else:
+			return render(request, 'datasources.html', {'form': form,})
+
+	else:
+		form = ListForm() # An unbound form
+
+		return render(request, 'datasources.html', {'form': form,}) 
