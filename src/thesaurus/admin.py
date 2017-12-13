3985ea03cd06213d6bfe204d140c19ebbd73d9ea
Markus Mandalka <github@mandalka.name>
2017-12-13 12:54:14 +0100

    Automatic rewrite of search UI facet config after save of (additional) facet

--- src/thesaurus/admin.py
+++ src/thesaurus/admin.py
@@ -7,18 +7,23 @@ from thesaurus.models import Group
 from thesaurus.models import GroupTag
 from thesaurus.models import Facet
 import thesaurus.views
+import ontologies.views
+
 
 class AlternateInline(admin.TabularInline):
     model = Alternate
     extra = 0
 
+
 class GroupTagInline(admin.TabularInline):
 	model = GroupTag
 	extra = 0
 
+
 class ConceptTagInline(admin.TabularInline):
 	model = ConceptTag
 	extra = 0
+
 
 class ConceptAdmin(admin.ModelAdmin):
 	list_display = ('prefLabel', 'alternates',)
@@ -34,6 +39,17 @@ class ConceptAdmin(admin.ModelAdmin):
 		thesaurus.views.tag_concept_and_message_stats(request=request, concept=obj)
 
 
+class FacetAdmin(admin.ModelAdmin):
+	list_display = ('facet', 'label',)
+
+	def save_model(self, request, obj, form, change):
+
+		super(FacetAdmin, self).save_model(request, obj, form, change)
+
+		# generate facet config for Solr-PHP-UI
+		ontologies.views.write_facet_config()
+
+
 class GroupAdmin(admin.ModelAdmin):
 	list_display = ('prefLabel', 'tags')
 	inlines = [ GroupTagInline, ]
@@ -43,5 +59,4 @@ class GroupAdmin(admin.ModelAdmin):
 admin.site.register(Concept, ConceptAdmin)
 admin.site.register(Alternate)
 admin.site.register(Group, GroupAdmin)
-
-admin.site.register(Facet)
+admin.site.register(Facet, FacetAdmin)
