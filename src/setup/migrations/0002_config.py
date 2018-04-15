f3bb4a970f73792383a810403403d13a3a7bf8f0
Markus Mandalka <github@mandalka.name>
2018-04-15 20:26:50 +0200

    Default setting of OCR dictionaries to English (OCR dictionary: eng)

--- src/setup/migrations/0002_config.py
+++ src/setup/migrations/0002_config.py
@@ -11,5 +11,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-	migrations.RunSQL("INSERT INTO setup_setup (id,title,description,language,languages,languages_force,ocr_languages,ocr,ocr_pdf,ocr_descew,ner_spacy,ner_stanford) VALUES (1,'default','Default config','en','en,de,fr,it,pt,nl,cz,ro,ar,fa','en','en',1,1,0,1,0);"),
+	migrations.RunSQL("INSERT INTO setup_setup (id,title,description,language,languages,languages_force,ocr_languages,ocr,ocr_pdf,ocr_descew,ner_spacy,ner_stanford) VALUES (1,'default','Default config','en','en,de,fr,it,pt,nl,cz,ro,ar,fa','en','eng',1,1,0,1,0);"),
     ]
