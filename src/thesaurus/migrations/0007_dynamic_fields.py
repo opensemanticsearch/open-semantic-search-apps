677ab32589397cad4fe89bd5b192fbd5f6f07274
Markus Mandalka <github@mandalka.name>
2018-05-01 22:54:50 +0200

    Harmonize facet names with Tika field names

---
+++ src/thesaurus/migrations/0007_dynamic_fields.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+	('thesaurus', '0001_initial'),
+	('thesaurus', '0002_facet_fields'),
+	('thesaurus', '0003_facets'),
+	('thesaurus', '0004_money'),
+	('thesaurus', '0005_hypothesis'),
+	('thesaurus', '0006_authors'),
+    ]
+
+    operations = [
+	migrations.RunSQL("UPDATE thesaurus_facet set facet='content_type_ss' WHERE facet='content_type'"),
+	migrations.RunSQL("UPDATE thesaurus_facet set facet='content_type_group_ss' WHERE facet='content_type_group'"),
+	migrations.RunSQL("UPDATE thesaurus_facet set facet='Message-From_ss' WHERE facet='message_from_ss'"),
+	migrations.RunSQL("UPDATE thesaurus_facet set facet='Message-To_ss' WHERE facet='message_to_ss'"),
+	migrations.RunSQL("UPDATE thesaurus_facet set facet='Message-CC_ss' WHERE facet='message_cc_ss'"),
+	migrations.RunSQL("UPDATE thesaurus_facet set facet='Message-BCC_ss' WHERE facet='message_bcc_ss'"),
+    ]
