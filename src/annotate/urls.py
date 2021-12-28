ee9fcf55dd9aac151c2371fdf758466572029595
Markus Mandalka <github@mandalka.name>
2021-12-28 14:32:43 +0100

    Export annotation to JSON

--- src/annotate/urls.py
+++ src/annotate/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
 	url(r'^$', views.IndexView.as_view(), name='index'),
 	url(r'^create$', views.create_annotation, name='create'),
 	url(r'^edit$', views.edit_annotation, name='edit'),
-	url(r'^rdf$', views.rdf, name='rdf'),
+	url(r'^json$', views.export_json, name='export_json'),
+	url(r'^rdf$', views.export_rdf, name='export_rdf'),
 	url(r'^(?P<pk>\d+)/update$', views.update_annotation, name='update'),
 	url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
 ]
--- src/annotate/views.py
+++ src/annotate/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse
-from django.http import HttpResponse 
+from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
 from django.forms import ModelForm
@@ -9,6 +9,7 @@ import django.forms as forms
 
 from opensemanticetl.etl import ETL
 
+import json
 import urllib
 from rdflib import Graph, Literal, BNode, Namespace, RDF, URIRef
 
@@ -27,10 +28,10 @@ def export_to_index(uri):
 	etl.config['plugins'] = ['enhance_rdf_annotations_by_http_request','enhance_multilingual']
 
 	parameters = etl.config.copy()
-	
+
 	# since we only enrich and not ETL the document/analysis pipeline again, text(s) has to be added to default search fields, not overwrite existing text
 	parameters['add'] = True
-	
+
 	# but the (maybe changed) comments should overwrite existing comments
 	# and some ETL process metadata in etl_* single value fields can not be added,
 	# so overwrite them, too
@@ -42,9 +43,9 @@ def export_to_index(uri):
 		'etl_enhance_multilingual_b',
 		'etl_enhance_multilingual_time_millis_i'
 	]
-	
+
 	parameters['id'] = uri
-	
+
 	etl.process (parameters=parameters, data={})
 	etl.commit()
 
@@ -67,14 +68,14 @@ class DetailView(generic.DetailView):
 class UpdateView(generic.UpdateView):
 	model = Annotation
 	form_class = AnnotationForm
-	
-	
-def update_annotation(request, id): 
-    instance = MyModel.objects.get(id=id)
-    form = MyForm(request.POST or None, instance=instance)
-    if form.is_valid():
-          form.save()
-          return redirect('next_view')
+
+
+def update_annotation(request, id):
+	instance = MyModel.objects.get(id=id)
+	form = MyForm(request.POST or None, instance=instance)
+	if form.is_valid():
+		form.save()
+		return redirect('next_view')
 
 
 def update_annotation(request, pk):
@@ -85,23 +86,23 @@ def update_annotation(request, pk):
 		form = AnnotationForm(request.POST, request.FILES, instance=annotation)
 		if form.is_valid():
 			annotation = form.save()
-			
+
 			# apply new data to search index
 			export_to_index(annotation.uri)
 
 			return HttpResponseRedirect( reverse('annotate:detail', args=[annotation.pk])) # Redirect after POST
 
 	else:
-			form = AnnotationForm(instance=annotation)
-			
-
-	return render(request, 'annotate/annotation_form.html', 
-			{'form': form, } )
+		form = AnnotationForm(instance=annotation)
 
 
+	return render(request, 'annotate/annotation_form.html',
+				  {'form': form, } )
 
-	
-	
+
+
+
+
 
 def create_annotation(request):
 
@@ -119,22 +120,22 @@ def create_annotation(request):
 
 		if 'uri' in request.GET:
 			initials = {
-					"uri": request.GET['uri']
+				"uri": request.GET['uri']
 			}
 			form = AnnotationForm(initial=initials)
 		else:
 			form = AnnotationForm()
-			
 
-	return render(request, 'annotate/annotation_form.html', 
-			{'form': form, } )
+
+	return render(request, 'annotate/annotation_form.html',
+				  {'form': form, } )
 
 
 
 def edit_annotation(request):
 
 	uri = request.GET['uri']
-	
+
 	annotations = Annotation.objects.filter(uri=uri)
 
 	if annotations:
@@ -142,25 +143,53 @@ def edit_annotation(request):
 		# get first primary key of annotation with given uri
 		pk = annotations[0].pk
 		annotation = Annotation.objects.get(pk=pk)
-	
+
 		#todo: warning message if more (if racing condition on creation)
-	
+
 		# redirect to edit this annotation
 		return HttpResponseRedirect( reverse('annotate:update', args=[pk])) # Redirect after POST
 
 	else:
 		# no annotation for that uri, so redirect to create view
 		return HttpResponseRedirect( "{}?uri={}".format( reverse('annotate:create'), urllib.parse.quote_plus( uri ) ) ) # Redirect after POST
-		
 
 
-# serialize tags for uri to RDF graph
-def rdf(request):
+
+# serialize annotations for uri to JSON
+def export_json(request):
 
 	uri = request.GET['uri']
-	
+
+	data = {}
+
+	annotations = Annotation.objects.filter(uri=uri)
+
+	for annotation in annotations:
+
+		if annotation.title:
+			data['title_txt'] = annotation.title
+
+		if annotation.notes:
+			data['notes_txt'] = annotation.notes
+
+		for tag in annotation.tags.all():
+			facet = "tag_ss"
+			if tag.facet:
+				facet = tag.facet.facet
+			if not facet in data:
+				data[facet] = []
+			data[facet].append(tag.prefLabel)
+
+	return HttpResponse(json.dumps(data), content_type="application/json")
+
+
+# serialize annotations for uri to RDF graph
+def export_rdf(request):
+
+	uri = request.GET['uri']
+
 	g = Graph()
-	
+
 	annotations = Annotation.objects.filter(uri=uri)
 
 	for annotation in annotations:
@@ -179,5 +208,5 @@ def rdf(request):
 			g.add( ( URIRef(annotation.uri), URIRef(facet_property), Literal(tag.prefLabel) ) )
 
 	status = HttpResponse(g.serialize( format='xml' ) )
-	status["Content-Type"] = "application/rdf+xml" 
+	status["Content-Type"] = "application/rdf+xml"
 	return status
